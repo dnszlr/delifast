ac2bbe7900263ca25c7704c15854da5efe111e98
kalash <mohammed_abdulwahab.kalash@student.reutlingen-university.de>
2021-05-13 15:39:14 +0200

    test

--- app/src/main/java/mobile_computing/delifast/MainActivity.java
+++ app/src/main/java/mobile_computing/delifast/MainActivity.java
@@ -2,13 +2,31 @@ package mobile_computing.delifast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.Timer;
+import java.util.TimerTask;
+import java.util.logging.Handler;
 
+public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        int timeout = 2000; // make the activity visible for 4 seconds
+
+        Timer timer = new Timer();
+        timer.schedule(new TimerTask() {
+
+            @Override
+            public void run() {
+                finish();
+                Intent homepage = new Intent(MainActivity.this, StartActivity.class);
+                startActivity(homepage);
+            }
+        }, timeout);
+
     }
 }

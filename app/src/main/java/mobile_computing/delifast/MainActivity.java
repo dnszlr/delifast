fa25f90c8bd8576a16885a2efe715b10ff2f4bf1
dennis <Dennis.Zhukki@gmail.com>
2021-05-16 15:30:34 +0200

    setup firestore database

--- app/src/main/java/mobile_computing/delifast/MainActivity.java
+++ app/src/main/java/mobile_computing/delifast/MainActivity.java
@@ -4,10 +4,6 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
-
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
-
 import java.util.Timer;
 import java.util.TimerTask;
 
@@ -15,7 +11,7 @@ import mobile_computing.delifast.authentication.AuthenticationActivity;
 import mobile_computing.delifast.others.DelifastConstants;
 
 public class MainActivity extends AppCompatActivity {
-    
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
--- app/src/main/java/mobile_computing/delifast/authentication/AuthenticationActivity.java
+++ app/src/main/java/mobile_computing/delifast/authentication/AuthenticationActivity.java
@@ -1,12 +1,6 @@
 package mobile_computing.delifast.authentication;
-
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
-
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
-
 import mobile_computing.delifast.R;
 import mobile_computing.delifast.others.DelifastConstants;
 

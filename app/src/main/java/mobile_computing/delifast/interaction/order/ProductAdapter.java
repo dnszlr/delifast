68cb254a8fb02286df068969d52890e1b4174401
Mohammed <mohammed_abdulwahab.kalash@student.reutlingen-university.de>
2021-05-21 18:10:37 +0200

    + Edit the adapter for the orders

--- app/src/main/java/mobile_computing/delifast/interaction/order/ProductAdapter.java
+++ app/src/main/java/mobile_computing/delifast/interaction/order/ProductAdapter.java
@@ -1,14 +1,19 @@
 package mobile_computing.delifast.interaction.order;
 
 import android.content.Context;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.lifecycle.ViewModelProvider;
+import androidx.lifecycle.ViewModelProviders;
 
 import java.util.ArrayList;
 
@@ -20,6 +25,13 @@ public class ProductAdapter extends ArrayAdapter<Product> {
     private Context context;
     private int resource;
     private ArrayList<Product> products;
+    // Declaration of the elements in a one row of the list
+    private ImageView imgProduct;
+    private TextView tvProductName;
+    private TextView tvProductCount;
+    private Button btnPlus;
+    private Button btnMinus;
+
 
     public ProductAdapter(@NonNull Context context, int resource, @NonNull ArrayList<Product> objects) {
         super(context, resource, objects);
@@ -36,11 +48,18 @@ public class ProductAdapter extends ArrayAdapter<Product> {
         LayoutInflater inflater = LayoutInflater.from(context);
         convertView = inflater.inflate(resource, parent, false);
 
-        TextView productNameTextView = convertView.findViewById(R.id.tvProductName);
-        productNameTextView.setText(product.getName());
+        tvProductName = convertView.findViewById(R.id.tvProductName);
+        tvProductName.setText(product.getName());
+
+        imgProduct = convertView.findViewById(R.id.imgProduct);
+        tvProductCount = convertView.findViewById(R.id.tvProductCount);
+        btnPlus = convertView.findViewById(R.id.btnPlus);
+
+        // TODO: implement the onClick method for the plus and minus bttons
 
         return convertView;
     }
+
 
     public void setProducts(ArrayList<Product> products) {
         this.products = products;

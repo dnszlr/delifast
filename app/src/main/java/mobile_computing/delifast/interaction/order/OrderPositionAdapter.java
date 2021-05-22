02bf7fb134cbc79064e4a86f5a4a3e4126567df1
Mohammed <mohammed_abdulwahab.kalash@student.reutlingen-university.de>
2021-05-22 10:17:29 +0200

    ~ ViewModel and Fragment for Order

--- app/src/main/java/mobile_computing/delifast/interaction/order/OrderPositionAdapter.java
+++ app/src/main/java/mobile_computing/delifast/interaction/order/OrderPositionAdapter.java
@@ -12,57 +12,64 @@ import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 import androidx.lifecycle.ViewModelProviders;
 
 import java.util.ArrayList;
 
 import mobile_computing.delifast.R;
+import mobile_computing.delifast.entities.OrderPosition;
 import mobile_computing.delifast.entities.Product;
 
-public class ProductAdapter extends ArrayAdapter<Product> {
+public class OrderPositionAdapter extends ArrayAdapter<OrderPosition> {
 
     private Context context;
     private int resource;
-    private ArrayList<Product> products;
+    private ArrayList<OrderPosition> orderPositions;
     // Declaration of the elements in a one row of the list
     private ImageView imgProduct;
     private TextView tvProductName;
     private TextView tvProductCount;
     private Button btnPlus;
     private Button btnMinus;
+    private OrderFragment fragment;
 
 
-    public ProductAdapter(@NonNull Context context, int resource, @NonNull ArrayList<Product> objects) {
+    public OrderPositionAdapter(@NonNull Context context, int resource, @NonNull ArrayList<OrderPosition> objects, OrderFragment fragment) {
         super(context, resource, objects);
         this.context = context;
         this.resource = resource;
-        this.products = objects;
+        this.orderPositions = objects;
+        this.fragment = fragment;
     }
 
     @NonNull
     @Override
     public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
-        Product product = getItem(position);
+        OrderPosition orderPosition = getItem(position);
 
         LayoutInflater inflater = LayoutInflater.from(context);
         convertView = inflater.inflate(resource, parent, false);
 
         tvProductName = convertView.findViewById(R.id.tvProductName);
-        tvProductName.setText(product.getName());
+        tvProductName.setText(orderPosition.getProduct().getName());
 
         imgProduct = convertView.findViewById(R.id.imgProduct);
         tvProductCount = convertView.findViewById(R.id.tvProductCount);
         btnPlus = convertView.findViewById(R.id.btnPlus);
 
+        tvProductCount.setText(orderPosition.getAmount());
+
         // TODO: implement the onClick method for the plus and minus bttons
+        btnPlus.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                fragment.editOrderPositionsInViewModel(position);
+            }
+        });
 
         return convertView;
     }
 
-
-    public void setProducts(ArrayList<Product> products) {
-        this.products = products;
-        notifyDataSetChanged();
-    }
 }
--- app/src/main/java/mobile_computing/delifast/interaction/order/OrderViewModel.java
+++ app/src/main/java/mobile_computing/delifast/interaction/order/OrderViewModel.java
@@ -1,11 +1,14 @@
 package mobile_computing.delifast.interaction.order;
 
+import android.util.Log;
+
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
 import java.util.ArrayList;
 import java.util.List;
 
+import mobile_computing.delifast.entities.OrderPosition;
 import mobile_computing.delifast.entities.Product;
 import mobile_computing.delifast.repositories.OrderRepository;
 import mobile_computing.delifast.repositories.ProductRepository;
@@ -16,11 +19,13 @@ public class OrderViewModel extends ViewModel {
     private OrderRepository orderRepository;
     private MutableLiveData<ArrayList<Product>> productList;
     private MutableLiveData<Boolean> result;
+    private MutableLiveData<ArrayList<OrderPosition>> orderPositionList;
 
     public OrderViewModel() {
         this.productRepository = new ProductRepository();
         this.productList = productRepository.getAll();
         this.result = new MutableLiveData<>();
+        this.orderPositionList = getAllPositions();
     }
 
     /**
@@ -30,5 +35,29 @@ public class OrderViewModel extends ViewModel {
      */
     public MutableLiveData<ArrayList<Product>> getAll() {
         return productRepository.getAll();
+    }
+
+    public MutableLiveData<ArrayList<OrderPosition>> getAllPositions() {
+        MutableLiveData<ArrayList<OrderPosition>> orderPositions = new MutableLiveData<>();
+        MutableLiveData<ArrayList<Product>> products = productRepository.getAll();
+        Log.d("Fucking Method:", "Product list length" + products.getValue().size());
+
+        for (Product product : products.getValue()) {
+            OrderPosition foundOp = orderPositions.getValue().stream().filter(position ->
+                    position.getProduct().equals(product)).findFirst().orElse(null);
+
+            if (foundOp == null) {
+                OrderPosition newOrderPosition = new OrderPosition(product, 0);
+                orderPositions.getValue().add(newOrderPosition);
+            }
+        }
+
+
+        return orderPositions;
+    }
+
+    public void changeCountOfOrderPosition(int orderPosition) {
+        OrderPosition op = this.orderPositionList.getValue().get(orderPosition);
+        op.setAmount(op.getAmount() + 1);
     }
 }

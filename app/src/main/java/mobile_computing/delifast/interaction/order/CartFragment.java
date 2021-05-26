78ea4164b37a602b1ecf9bed33befae1060d3aa2
Mohammed <mohammed_abdulwahab.kalash@student.reutlingen-university.de>
2021-05-26 19:14:19 +0200

    + Payment with PayPal + fragment delivery Layout ~ Bug fixes in the order activity

--- app/src/main/java/mobile_computing/delifast/interaction/order/CartFragment.java
+++ app/src/main/java/mobile_computing/delifast/interaction/order/CartFragment.java
@@ -1,5 +1,6 @@
 package mobile_computing.delifast.interaction.order;
 
+import android.app.Activity;
 import android.app.DatePickerDialog;
 import android.app.TimePickerDialog;
 import android.content.Intent;
@@ -15,6 +16,7 @@ import android.text.Editable;
 import android.text.InputType;
 import android.text.TextWatcher;
 import android.util.Log;
+import android.util.Patterns;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -36,9 +38,17 @@ import com.mapbox.api.geocoding.v5.models.CarmenFeature;
 import com.mapbox.geojson.Point;
 import com.mapbox.mapboxsdk.plugins.places.autocomplete.PlaceAutocomplete;
 import com.mapbox.mapboxsdk.plugins.places.autocomplete.model.PlaceOptions;
+import com.paypal.android.sdk.payments.PayPalConfiguration;
+import com.paypal.android.sdk.payments.PayPalPayment;
+import com.paypal.android.sdk.payments.PayPalPaymentDetails;
+import com.paypal.android.sdk.payments.PayPalService;
+import com.paypal.android.sdk.payments.PaymentActivity;
+import com.paypal.android.sdk.payments.PaymentConfirmation;
 
 import org.json.JSONException;
 
+import java.math.BigDecimal;
+import java.math.RoundingMode;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Calendar;
@@ -52,12 +62,17 @@ import mobile_computing.delifast.others.DelifastConstants;
 public class CartFragment extends Fragment {
 
     private TextInputEditText etUserDeposit, etSupplyPrice, etServiceFee, etAddress, etDeadline;
+    private TextView tvCartSum;
     private MaterialButton btnPay;
     private OrderViewModel model;
     private LinearLayout lvContentLayout;
     private CarmenFeature reutlingen;
     private CarmenFeature berlin;
     private SimpleDateFormat simpleDateFormat;
+
+    private static PayPalConfiguration paypalConfig = new PayPalConfiguration()
+            .environment(PayPalConfiguration.ENVIRONMENT_NO_NETWORK)
+            .clientId(DelifastConstants.PAYPAL_CLIENT_ID);
 
     public CartFragment() {
         // Required empty public constructor
@@ -66,6 +81,12 @@ public class CartFragment extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public void onDestroy() {
+        getContext().stopService(new Intent(getContext(), PayPalService.class));
+        super.onDestroy();
     }
 
     @Override
@@ -85,6 +106,12 @@ public class CartFragment extends Fragment {
                 updateUI(order);
             }
         });
+
+        // Start PayPal Service
+        Intent paypalIntent = new Intent(getContext(), PayPalService.class);
+        paypalIntent.putExtra(PayPalService.EXTRA_PAYPAL_CONFIGURATION, paypalConfig);
+        getContext().startService(paypalIntent);
+
         return cartView;
     }
 
@@ -168,8 +195,12 @@ public class CartFragment extends Fragment {
 
         }
 
+        etUserDeposit.setError(null);
+        etAddress.setError(null);
+        etDeadline.setError(null);
         etServiceFee.setText(String.valueOf(order.getServiceFee()));
         etUserDeposit.setText(String.valueOf(order.getUserDeposit()));
+        tvCartSum.setText(String.valueOf(roundDouble(order.getUserDeposit() + order.getServiceFee())));
         Log.d("Vorkasse: ", "Deposit" + order.getUserDeposit());
         if (order.getDeadline() != null) {
             etDeadline.setText(simpleDateFormat.format(order.getDeadline()));
@@ -177,12 +208,52 @@ public class CartFragment extends Fragment {
         if (order.getCustomerAddress() != null) {
             etAddress.setText(order.getCustomerAddress().getAddressString());
         }
+
+        btnPay.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(isReadyToPay(Double.parseDouble(tvCartSum.getText().toString().trim()),
+                        etAddress.getText().toString().trim(),
+                        etDeadline.getText().toString().trim())){
+                    processPayment(order);
+                }
+
+            }
+        });
     }
 
 
     @Override
     public void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
+
+        Log.d("PayPalTag: ", "request ------->" + requestCode + ", result ------> " + resultCode);
+        if(requestCode == DelifastConstants.PAYMENT_REQUEST_CODE){
+            if(resultCode == Activity.RESULT_OK){
+                PaymentConfirmation confirmation = data.getParcelableExtra(PaymentActivity.EXTRA_RESULT_CONFIRMATION);
+                if(confirmation != null){
+                    try {
+                        String paymentDetails = confirmation.toJSONObject().toString(4);
+                        Log.d("PayPalTag: ", "Details ------->" + paymentDetails);
+                        Toast.makeText(getContext(), "Sie haben erfolgreich bezahlt", Toast.LENGTH_SHORT).show();
+
+                    } catch (JSONException e) {
+                        Toast.makeText(getContext(), "JSON Error", Toast.LENGTH_SHORT).show();
+                        e.printStackTrace();
+                    }
+                }
+            }
+            else if (resultCode == Activity.RESULT_CANCELED) {
+                Toast.makeText(getContext(), "Paypal Activity canceled", Toast.LENGTH_SHORT).show();
+            }
+            else if(resultCode == PaymentActivity.RESULT_EXTRAS_INVALID){
+                Toast.makeText(getContext(), "Paypal Activity Invalid", Toast.LENGTH_SHORT).show();
+            }
+            else {
+                Log.d("PayPalTag: ", "Details ------->" + resultCode);
+            }
+        }
+
         if (resultCode == AutocompleteActivity.RESULT_OK && requestCode == 100) {
             CarmenFeature feature = PlaceAutocomplete.getPlace(data);
             try {
@@ -198,6 +269,7 @@ public class CartFragment extends Fragment {
             Status status = Autocomplete.getStatusFromIntent(data);
             Toast.makeText(getActivity().getApplicationContext(), status.getStatusMessage(), Toast.LENGTH_SHORT).show();
         }
+
     }
 
     /**
@@ -212,6 +284,7 @@ public class CartFragment extends Fragment {
         etUserDeposit = cartView.findViewById(R.id.etUserDeposit);
         etServiceFee = cartView.findViewById(R.id.etServiceFee);
         etAddress = cartView.findViewById(R.id.etAddress);
+        tvCartSum = cartView.findViewById(R.id.tvCartSum);
         btnPay = cartView.findViewById(R.id.btnPay);
     }
 
@@ -269,5 +342,59 @@ public class CartFragment extends Fragment {
      */
     public void deleteOrderPositionInViewModel(OrderPosition orderPosition){
         model.deleteOrderPositionFromOrder(orderPosition);
+    }
+
+
+    /**
+     * Round a double value to a value with only
+     * 2 positions after the comma
+     *
+     * @param value: the double value to be rounded
+     */
+    public static double roundDouble(double value) {
+        int places = 2;
+        BigDecimal bd = BigDecimal.valueOf(value);
+        bd = bd.setScale(places, RoundingMode.HALF_UP);
+        return bd.doubleValue();
+    }
+
+    private void processPayment(Order order) {
+        double sumToPay = order.getUserDeposit() + order.getServiceFee();
+
+        PayPalPayment paypalPayment = new PayPalPayment(new BigDecimal(String.valueOf(sumToPay)), "USD", "Zahlung für Delifast", PayPalPayment.PAYMENT_INTENT_SALE);
+        Intent intent = new Intent(getContext(), PaymentActivity.class);
+        intent.putExtra(PayPalService.EXTRA_PAYPAL_CONFIGURATION, paypalConfig);
+        intent.putExtra(PaymentActivity.EXTRA_PAYMENT, paypalPayment);
+        startActivityForResult(intent, DelifastConstants.PAYMENT_REQUEST_CODE);
+
+    }
+
+
+    /**
+     * Check if all required details for the order
+     * are available (adress, price > 0 and time)
+     *
+     * @param sum: the order price
+     *        adress: the order adress
+     *        dateTime: the required time of order
+     *
+     */
+    private boolean isReadyToPay(double sum, String adress, String dateTime) {
+        if (sum < 1) {
+            etUserDeposit.setError(DelifastConstants.MISSING_PRODUCTS);
+            etUserDeposit.requestFocus();
+            return false;
+        }
+        if (adress.isEmpty()) {
+            etAddress.setError(DelifastConstants.MISSING_ADRESS);
+            etAddress.requestFocus();
+            return false;
+        }
+        if (dateTime.isEmpty()) {
+            etDeadline.setError(DelifastConstants.MISSING_DEADLINE);
+            etDeadline.requestFocus();
+            return false;
+        }
+        return true;
     }
 }
--- app/src/main/java/mobile_computing/delifast/others/DelifastConstants.java
+++ app/src/main/java/mobile_computing/delifast/others/DelifastConstants.java
@@ -11,6 +11,11 @@ public class DelifastConstants {
     public final static String INVALIDPASSWORD = "Passwort muss min. 8 Zeichen haben";
     public final static int RC_SIGN_IN = 123;
 
+    // CartFragment
+    public final static String MISSING_ADRESS = "Adresse erforderlich";
+    public final static String MISSING_DEADLINE = "Zeit erforderlich";
+    public final static String MISSING_PRODUCTS = "Keine Produkte ausgewählt";
+
     // Collections
     public final static String USERCOLLECTION = "users";
     public final static String PRODUCTCOLLECTION = "products";
@@ -25,4 +30,10 @@ public class DelifastConstants {
     // Mapbox API key
     public static final String MAPBOX_ACCESS_TOKEN = "sk.eyJ1IjoibWthbGFzaCIsImEiOiJja3AyYWVsNm0xMjltMndsZ3FqZXhnZG11In0.G0zqmJ50IGR31LpPx82LNg";
     public static final String TIMEFORMAT = "dd.MM.yy HH:mm";
+
+    // PayPal Client ID
+    public static final String PAYPAL_CLIENT_ID = "AS0mPsf2ozBPnHYgmZNKVtQqoeteJQTp9K6JtAxQ2jYnrXnIV5i-Ui0qwQOo_x9N48AOt0pZ3e3SBHRX";
+    public static final String PAYPAL_CLIENT_ID_2 = "AUQ4BVg6XC1gjyleBEoqZuxz7mEaOq_4HiUaZcljW8uPb3yIwmQkb-Tgs6CFECCFJI1lJbKqCjdSBYob";
+    public final static int PAYMENT_REQUEST_CODE = 7171;
+
 }

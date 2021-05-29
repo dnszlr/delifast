f21f041d3e2b50549b7c354841c46086b0b9c3c9
Mohammed <mohammed_abdulwahab.kalash@student.reutlingen-university.de>
2021-05-29 14:01:13 +0200

    + Layout for the profile fragment

--- app/src/main/java/mobilecomputing/delifast/interaction/profile/ProfileFragment.java
+++ app/src/main/java/mobilecomputing/delifast/interaction/profile/ProfileFragment.java
@@ -1,5 +1,8 @@
 package mobilecomputing.delifast.interaction.profile;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -7,10 +10,18 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
 
+import com.google.firebase.auth.FirebaseAuth;
+
+import mobilecomputing.delifast.MainActivity;
 import mobilecomputing.delifast.R;
+import mobilecomputing.delifast.authentication.AuthenticationActivity;
 
 public class ProfileFragment extends Fragment {
+
+    private ImageView btnLogout;
 
     public ProfileFragment() {
         // Required empty public constructor
@@ -19,7 +30,49 @@ public class ProfileFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_profile, container, false);
+        View profileView = inflater.inflate(R.layout.fragment_profile, container, false);
+
+        initView(profileView);
+
+        return profileView;
+    }
+
+    private void initView(View profileView) {
+        btnLogout = profileView.findViewById(R.id.btnProfileLogout);
+        setOnClickListenerForLogout();
+    }
+
+    private void setOnClickListenerForLogout() {
+        btnLogout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                AlertDialog dialog = new AlertDialog.Builder(getActivity())
+                        .setTitle("Logout")
+                        .setMessage("Möchten Sie sich wirklich abmelden?")
+                        .setPositiveButton("Logout", null)
+                        .setNegativeButton("abbrechen", null)
+                        .create();
+
+                dialog.setOnShowListener(new DialogInterface.OnShowListener() {
+
+                    @Override
+                    public void onShow(DialogInterface dialogInterface) {
+
+                        Button button = dialog.getButton(AlertDialog.BUTTON_POSITIVE);
+                        button.setOnClickListener(new View.OnClickListener() {
+                            @Override
+                            public void onClick(View view) {
+                                Intent loginPage = new Intent(getActivity(), AuthenticationActivity.class);
+                                startActivity(loginPage);
+                                getActivity().finish();
+                                FirebaseAuth.getInstance().signOut();
+                                dialog.dismiss();
+                            }
+                        });
+                    }
+                });
+                dialog.show();
+            }
+        });
     }
 }

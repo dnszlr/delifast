8365e6afad73be454040efe8bad955d1164cd827
Mohammed <mohammed_abdulwahab.kalash@student.reutlingen-university.de>
2021-05-30 15:01:25 +0200

    + download profile pics from firebase

--- app/src/main/java/mobilecomputing/delifast/interaction/profile/ProfileUserDataFragment.java
+++ app/src/main/java/mobilecomputing/delifast/interaction/profile/ProfileUserDataFragment.java
@@ -4,11 +4,13 @@ import android.app.Activity;
 import android.app.ProgressDialog;
 import android.content.Intent;
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.net.Uri;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.appcompat.content.res.AppCompatResources;
 import androidx.cardview.widget.CardView;
 import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.fragment.app.Fragment;
@@ -26,6 +28,7 @@ import android.widget.Toast;
 import com.bumptech.glide.Glide;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
 import com.google.android.material.snackbar.Snackbar;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
@@ -142,8 +145,33 @@ public class ProfileUserDataFragment extends Fragment {
     }
 
     private void downloadPicture(String uId){
-        StorageReference storageReference = storage.getReference();
-        StorageReference pathReference = storageReference.child("images/" + uId + ".jpeg");
-        Glide.with(this).load(pathReference).into(profilePic);
+        storageReference.child("images/" + uId).getBytes(Long.MAX_VALUE)
+                .addOnSuccessListener(new OnSuccessListener<byte[]>() {
+                    @Override
+                    public void onSuccess(byte[] bytes) {
+                        Bitmap bmp= BitmapFactory.decodeByteArray(bytes,0,bytes.length);
+                        profilePic.setImageBitmap(bmp);
+                    }})
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        try {
+                            throw e;
+                        } catch(Exception imageException) {
+                            Toast.makeText(getContext(), "Das Bild wurde NICHT geladen.", Toast.LENGTH_SHORT).show();
+                        }
+                        Log.d("Scheiss Error: ", "Hier: " + e);
+                        Toast.makeText(getContext(), "Das Bild wurde NICHT geladen.", Toast.LENGTH_SHORT).show();
+
+                    }
+                });
+        /*
+        if(FirebaseStorage.getInstance().getReference() != null){
+            Glide.with(this).load(pathReference).into(profilePic)
+                    .onLoadFailed(AppCompatResources.getDrawable(getContext(), R.drawable.ic_baseline_location_on_24));
+        }
+        */
+
+
     }
 }

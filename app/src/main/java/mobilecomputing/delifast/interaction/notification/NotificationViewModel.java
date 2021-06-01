f817237570fe0ccd056d429be26539fe43e69701
dennis <Dennis.Zhukki@gmail.com>
2021-06-01 11:34:07 +0200

    + implemented NotificationViewModel

--- app/src/main/java/mobilecomputing/delifast/interaction/notification/NotificationViewModel.java
+++ app/src/main/java/mobilecomputing/delifast/interaction/notification/NotificationViewModel.java
@@ -1,6 +1,28 @@
 package mobilecomputing.delifast.interaction.notification;
 
+import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
+import java.util.List;
+
+import mobilecomputing.delifast.entities.Notification;
+import mobilecomputing.delifast.repositories.NotificationRepository;
+
 public class NotificationViewModel extends ViewModel {
+
+    private NotificationRepository notificationRepository;
+
+    public NotificationViewModel() {
+        this.notificationRepository = new NotificationRepository();
+    }
+
+    public void save(Notification notification) {
+        if (notification != null) {
+            notificationRepository.save(notification);
+        }
+    }
+
+    public MutableLiveData<List<Notification>> getAllByUserId(String userId) {
+        return notificationRepository.getAllByUserId(userId);
+    }
 }
